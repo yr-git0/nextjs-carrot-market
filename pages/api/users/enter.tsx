f10b0d555d15f76178a09554de591b5c87710c2d
Yaerin Park <rnrmf941@gmail.com>
2023-10-30 22:42:31 +0900

    👻Comment: 불필요한 주석 삭제

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -6,17 +6,5 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
   res.json({ ok: true });
 }
-// {
-//   await client.user.create({
-//     data: {
-//       email: "hi@email.com",
-//       name: "hi",
-//     },
-//   });
-
-//   res.json({
-//     ok: true,
-//   });
-// }
 
 export default withHandler("POST", handler);
