b6c7a39d07c7b35cfd19ff6a56c3966c8c80d0d9
Yaerin Park <rnrmf941@gmail.com>
2023-10-31 01:08:54 +0900

    😎Feat:check Account logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,10 +1,53 @@
-// import client from "@/libs/client";
+import client from "@/libs/client";
 import withHandler from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  res.json({ ok: true });
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+  /* if (phone) {
+    user = await client.user.findUnique({
+      where: {
+        phone: +phone,
+      },
+    });
+    if (!user) {
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          phone: +phone,
+        },
+      });
+    }
+  }
+  if (email) {
+    user = await client.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          email,
+        },
+      });
+    }
+  } */
+  console.log(user);
+  return res.status(200).end();
 }
 
 export default withHandler("POST", handler);
