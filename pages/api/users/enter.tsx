2c14a168065852ec65a23a0edd12645f414fbee7
Yaerin Park <rnrmf941@gmail.com>
2023-11-01 00:41:53 +0900

    😎Feat:SMS & Email 인증메일 전송 구현

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,13 +1,19 @@
 import client from "@/libs/client";
+import smtpTransport from "@/libs/server/email";
 import withHandler, { ResponseType } from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+import twilio from "twilio";
 
+const twilioClient = twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
   const { phone, email } = req.body;
-  const user = phone ? { phone: +phone } : { email };
+  const user = phone ? { phone } : { email };
   if (!user) return res.status(400).json({ ok: false });
 
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
@@ -40,6 +46,40 @@ async function handler(
     },
   });
 
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MESSAGING_SERVICE_SID,
+      from: process.env.TWILIO_FROM_NUMBER,
+      to: process.env.MY_NUMBER!,
+      body: `Your login token is ${payload}`,
+    });
+    console.log(message);
+  }
+
+  if (email) {
+    const mailOptions = {
+      from: process.env.MAIL_ID,
+      to: email,
+      subject: "Nomad Carrot Authentication Email",
+      text: `Authentication Code: ${payload}`,
+      html: `<strong>Your token is ${payload}`,
+    };
+    const result = await smtpTransport.sendMail(
+      mailOptions,
+      (error, responses) => {
+        if (error) {
+          console.log(error);
+          return null;
+        } else {
+          console.log(responses);
+          return null;
+        }
+      }
+    );
+    smtpTransport.close();
+    console.log(result);
+  }
+
   /* if (phone) {
     user = await client.user.findUnique({
       where: {
@@ -70,7 +110,6 @@ async function handler(
       });
     }
   } */
-  console.log(token);
   return res.json({
     ok: true,
   });
