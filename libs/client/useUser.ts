80811ff786d08c56ffd20594df92810dde4291d4
Yaerin Park <rnrmf941@gmail.com>
2023-11-04 11:07:55 +0900

    🔰Refactor: useUsers 훅 리팩토링(1)

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,19 +1,31 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+const fetcher = (url: string) => {
+  fetch(url).then((response) => response.json());
+};
 
 export default function useUser() {
-  const [user, setUser] = useState();
+  const { data, error } = useSWR("/api/users/me", fetcher);
   const router = useRouter();
   useEffect(() => {
-    fetch("/api/users/me")
-      .then((response) => response.json())
-      .then((data) => {
-        if (!data.ok) {
-          return router.replace("/enter");
-        }
-        setUser(data.profile);
-      });
-  }, [router]);
+    if (data && !data.ok) {
+      router.replace("/enter");
+    }
+  }, [data, router]);
+  //   const [user, setUser] = useState();
+  //   useEffect(() => {
+  //     fetch("/api/users/me")
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         if (!data.ok) {
+  //           return router.replace("/enter");
+  //         }
+  //         setUser(data.profile);
+  //       });
+  //   }, [router]);
 
-  return user;
+  //   return user;
+  return { user: data?.profile, isLoading: !data && !error };
 }
