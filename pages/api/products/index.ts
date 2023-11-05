cc8fe4778c7a330f3c10a1184d98a8007294eb14
Yaerin Park <rnrmf941@gmail.com>
2023-11-05 23:52:14 +0900

    😎Feat: upload Product 기능 구현

---
+++ pages/api/products/index.ts
@@ -0,0 +1,38 @@
+import client from "@/libs/client";
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import { withApiSession } from "@/libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    body: { name, price, description },
+    session: { user },
+  } = req;
+  const product = await client.product.create({
+    data: {
+      name,
+      price: +price,
+      description,
+      image: "xxx",
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+    },
+  });
+  res.json({
+    ok: true,
+    product,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    method: "POST",
+    handler,
+  })
+);
